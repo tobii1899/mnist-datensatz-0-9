750271799e5ec29590ca5965bfb43009c634c345
Tobias <t.vieghofer@students.htl-leonding.ac.at>
2026-09-16 22:01:16 +0200

    Fix model error

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 from keras.datasets import mnist
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import layers, models
 from tensorflow.keras.callbacks import EarlyStopping
@@ -43,14 +43,13 @@ def forward(X_train, Y_train):
     ])
 
     model = Sequential([
-        layers.Input(shape=(28, 28, 1)),
-        layers.Conv2D(32, (3, 3), activation='relu'),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(64, (3, 3), activation='relu'),
-        layers.MaxPooling2D((2, 2)),
-        layers.Flatten(),
-        layers.Dense(64, activation='relu'),
-        layers.Dense(10, activation='softmax')
+        Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        MaxPooling2D((2, 2)),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Flatten(),
+        Dense(64, activation='relu'),
+        Dense(10, activation='softmax')
     ])
 
     model.compile(
